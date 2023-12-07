0c2862535f0e32225fd3cc8f30d684261c1d3960
nniyazaliev <niyazaliev.dev@gmail.com>
2023-12-07 12:45:06 +0600

    null safety

--- lib/weather_main_page.dart
+++ lib/weather_main_page.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+
+import 'package:app_09/geolocator_sevices.dart';
+import 'package:app_09/weather_services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -9,81 +13,130 @@ class WeatherMainPage extends StatefulWidget {
 }
 
 class _WeatherMainPageState extends State<WeatherMainPage> {
+  String? name;
+  Map<String, dynamic> weatherData = {};
+
+  @override
+  void didChangeDependencies() async {
+    log('name - $name');
+    await getWeatherByLocation();
+    super.didChangeDependencies();
+  }
+
+  Future<void> getWeatherByLocation() async {
+    final position = await GeolocatorServices().getCurrentLocation();
+
+    if (position != null) {
+      weatherData = await WeatherServices().getWeatherByLocation(position);
+
+      setState(() {
+        name = 'Azamat';
+      });
+    } else {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/city.jpg"),
             fit: BoxFit.cover,
           ),
         ),
         child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  /* horizontal: 8.0, */
-                  vertical: 18,
-                ),
-                child: Row(
+          child: weatherData.isEmpty
+              ? const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.white,
+                  ),
+                )
+              : Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    TextButton.icon(
-                      onPressed: () {},
-                      icon: SvgPicture.asset(
-                        'assets/vectors/geo.svg',
-                        color: Colors.white,
-                        height: 45,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0,
+                        vertical: 18,
                       ),
-                      label: SizedBox(),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton.icon(
+                            onPressed: () async {
+                              await getWeatherByLocation();
+                            },
+                            icon: SvgPicture.asset(
+                              'assets/vectors/geo.svg',
+                              color: Colors.white,
+                              height: 45,
+                            ),
+                            label: SizedBox(),
+                          ),
+                          TextButton.icon(
+                            onPressed: () {},
+                            icon: SvgPicture.asset(
+                              'assets/vectors/city.svg',
+                              color: Colors.white,
+                              height: 45,
+                            ),
+                            label: SizedBox(),
+                          ),
+                        ],
+                      ),
                     ),
-                    TextButton.icon(
-                      onPressed: () {},
-                      icon: SvgPicture.asset(
-                        'assets/vectors/city.svg',
-                        color: Colors.white,
-                        height: 45,
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 18.0),
+                      child: Text(
+                        '${(weatherData['main']['temp'] as double).toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontSize: 60,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                      label: SizedBox(),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 18.0,
+                        vertical: 40,
+                      ),
+                      child: Text(
+                        '${(weatherData['weather'][0]['description'])}',
+                        style: TextStyle(
+                          fontSize: 34,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ],
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 18.0),
-                    child: Text(
-                      '7C ICON',
-                      style: TextStyle(
-                        fontSize: 60,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 18.0),
-                    child: Text(
-                      '7C ICON',
-                      style: TextStyle(
-                        fontSize: 34,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
         ),
       ),
     );
+  }
+
+  Text _getTextName() {
+    if (name == null) {
+      return const Text(
+        'null',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+        ),
+      );
+    } else {
+      return Text(
+        name!,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+        ),
+      );
+    }
   }
 }
--- lib/weather_services.dart
+++ lib/weather_services.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:app_09/convert_units_services.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 
 const String apiKey = '902f5e6878f4f3150d881238c5078e50';
@@ -18,6 +19,29 @@ class WeatherServices {
     if (responce.statusCode == 200) {
       final Map<String, dynamic> data =
           jsonDecode(responce.body) as Map<String, dynamic>;
+
+      /// openweathermap.org returns temperature - Kelvin,
+      /// we are getting Celcius from Kelvin
+      data['main']['temp'] = ConvertUnitsServices()
+          .kelvinToCelcius(data['main']['temp'] as double)
+          .roundToDouble();
+
+      return data;
+    } else {
+      return {};
+    }
+  }
+
+  Future<Map<String, dynamic>> getWeatherByLocation(Position position) async {
+    final uri = Uri.parse(
+        '${url}lat=${position.latitude}&lon=${position.longitude}&appid=$apiKey');
+    final http.Response responce = await http.get(uri);
+
+    if (responce.statusCode == 200) {
+      final Map<String, dynamic> data =
+          jsonDecode(responce.body) as Map<String, dynamic>;
+
+      log('responce.body = ${responce.body}');
 
       /// openweathermap.org returns temperature - Kelvin,
       /// we are getting Celcius from Kelvin
